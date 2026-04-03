2161551f70c18343759e7eda69185f753b0de9b8
codercoder61 <mohamedknoud@gmail.com>
2026-04-03 18:13:56 +0200

    Update scraper2.js

--- scraper2.js
+++ scraper2.js
@@ -23,7 +23,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
