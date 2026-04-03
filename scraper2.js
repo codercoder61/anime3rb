aa1c5ae631169a77f8fa19a417e906d2dee47121
codercoder61 <mohamedknoud@gmail.com>
2026-04-03 20:24:31 +0200

    Update scraper2.js

--- scraper2.js
+++ scraper2.js
@@ -33,11 +33,15 @@ app.get("/image-proxy", async (req, res) => {
     const url = req.query.url;
     if (!url) return res.status(400).send("url query param required");
 
-    const response = await fetch(url, { 
-      headers: {
-        "User-Agent": "Mozilla/5.0" // Some servers block default fetch UA
-      }
-    });
+    const response = await fetch(url, {
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+          "Accept": "image/avif,image/webp,image/apng,image/*,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9",
+          "Referer": "https://anime3rb.com/", // VERY IMPORTANT
+          "Connection": "keep-alive"
+        }
+      });
 
     if (!response.ok) {
       return res.status(response.status).send("Failed to fetch image");
